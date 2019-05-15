f8d51b4f16e064e3a39bf06d9c1b71aa763f0e3e
Guadalupe Fernandez-Nunez <gf347@drexel.edu>
2019-05-15 15:27:03 -0400

    Added reference to classes package

--- ontime/ontime/settings.py
+++ ontime/ontime/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
-    'accounts.apps.ClassesConfig',
+    'classes.apps.ClassesConfig',
 ]
 
 MIDDLEWARE = [
