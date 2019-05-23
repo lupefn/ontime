b593c57ff2c2ea5521181275e16a10e263d4ced1
Guadalupe Fernandez-Nunez <gf347@drexel.edu>
2019-05-23 00:02:03 -0400

    Registered Course and CourseInstanceModels

--- ontime/classes/admin.py
+++ ontime/classes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from classes.models import Courses, CourseInstance
 
 # Register your models here.
+admin.site.register(Courses)
+admin.site.register(CourseInstance)
